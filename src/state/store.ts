db2230e00c1d6fb6b881153c0f588ca3a908c3a0
Jad alKhawand <jkh42005@gmail.com>
2024-01-03 14:29:49 +0200

    incrementing by 10

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,18 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counter/CounterSlice";
-
-interface RootState {
-  counter: {
-    value: number;
-  };
-}
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    // Add other reducers here if you have them
-  },
-});
+  reducer: {},
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootStateType = ReturnType<typeof store.getState>;
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
